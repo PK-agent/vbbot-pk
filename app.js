0f07056c34c836bfdfc84d138edbb868755b525f
PK-agent <zayar9786@gmail.com>
2020-10-14 01:12:25 +0630

    add order

--- app.js
+++ app.js
@@ -146,47 +146,6 @@ app.post('/register',function(req,res){
        
 });
 
-app.post('/register',function(req,res){   
-    
-    currentUser.name = req.body.name;
-    currentUser.phone = req.body.phone;
-    currentUser.address = req.body.address;
-
-    let data = {
-        viberid: currentUser.id,
-        name: currentUser.name,
-        phone: currentUser.phone,
-        address: currentUser.address
-    }
-
-   
-    db.collection('users').add(data)
-    .then(()=>{
-            let data = {
-                   "receiver":currentUser.id,
-                   "min_api_version":1,
-                   "sender":{
-                      "name":"PyaungKyi",
-                      "avatar":"http://api.adorable.io/avatar/200/isitup"
-                   },
-                   "tracking_data":"tracking data",
-                   "type":"text",
-                   "text": "Thank you!"+req.body.name
-                }                
-
-                fetch('https://chatapi.viber.com/pa/send_message', {
-                    method: 'post',
-                    body:    JSON.stringify(data),
-                    headers: { 'Content-Type': 'application/json', 'X-Viber-Auth-Token': process.env.AUTH_TOKEN },
-                })
-                .then(res => res.json())
-                .then(json => console.log('JSON', json))
-
-    }).catch((error)=>{
-        console.log('ERROR:', error);
-    });
-       
-});
 
 app.get('/customerOrder', async(req,res) => {  
     const usersRef = db.collection('users');
