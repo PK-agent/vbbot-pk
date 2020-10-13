361b36653a511e4d371d2b74e180c419e5e44c88
PK-agent <zayar9786@gmail.com>
2020-10-14 01:19:10 +0630

    add order

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ app.post('/register',function(req,res){
 
 app.get('/customerOrder', async(req,res) => {  
     const usersRef = db.collection('users');
-    const snapshot = await userRef.where('viberid', '==', currentUser.id).limit(1).get();
+    const snapshot = await usersRef.where('viberid', '==', currentUser.id).limit(1).get();
     console.log(snapshot);
     // const snapshot = await usersRef.get();
     if (snapshot.empty) {
