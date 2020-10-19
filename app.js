6f4ac2fd23d51b1ef1c854ade1b8c03893378f1b
PK-agent <zayar9786@gmail.com>
2020-10-19 19:55:39 +0630

    merchant entry list

--- app.js
+++ app.js
@@ -322,7 +322,7 @@ app.get('/admin/merchant/entrylist', async (req,res) => {
         data.push(user);        
     });   
     
-    res.render('merch-entryList.ejs', {user:user});
+    res.render('merch-entryList.ejs', {data:data});
 });
 
 
