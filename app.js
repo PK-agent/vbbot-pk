7988323200ec4876a3f16024f4bca7a3d1afd455
Htet Aung Khant <chitkogyi19950@gmail.com>
2020-10-21 00:45:02 +0630

    htet aung khant

--- app.js
+++ app.js
@@ -354,26 +354,26 @@ app.get('/admin/merchant/entrylist', async (req,res) => {
     
     let data = [];
     userSnapshot.forEach( async doc => {
-        const ordersRef = db.collection('users').doc(doc.id).collection('orders'); 
-        const ordersSnapshot = await ordersRef.get();
-        if(ordersSnapshot.empty) {
-            console.log('No matching documents.');
-            return;
-        }
-        ordersSnapshot.forEach(doc1 => {
-            let user = {};
-            user.id = doc1.id;
-            user.name = doc1.data().name;
-            user.phone = doc1.data().phone;         
-            user.address = doc1.data().address;
-            user.corn_type = doc1.data().corn_type;
-            user.corn_qty = doc1.data().corn_qty;
-            user.wanted_price = doc1.data().wanted_price;
-            user.comment = doc1.data().comment;
-            user.received_date = doc1.data().received_date;   
-            console.log(user);
-            data.push(user);  
-        });                   
+        data.push(doc.id);
+        // const ordersRef = db.collection('users').doc(doc.id).collection('orders'); 
+        // const ordersSnapshot = await ordersRef.get();
+        // if(ordersSnapshot.empty) {
+        //     console.log('No matching documents.');
+        //     return;
+        // }
+        // ordersSnapshot.forEach(doc1 => {
+        //     let user = {};
+        //     user.id = doc1.id;
+        //     user.name = doc1.data().name;
+        //     user.phone = doc1.data().phone;         
+        //     user.address = doc1.data().address;
+        //     user.corn_type = doc1.data().corn_type;
+        //     user.corn_qty = doc1.data().corn_qty;
+        //     user.wanted_price = doc1.data().wanted_price;
+        //     user.comment = doc1.data().comment;
+        //     user.received_date = doc1.data().received_date;   
+        //     data.push(user);  
+        // });                   
     });
     res.render('merch-entryList.ejs', {data});    
 });
