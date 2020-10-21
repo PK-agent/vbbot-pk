352b6f7fd3ff95803150374e084c4afb3267d954
Htet Aung Khant <chitkogyi19950@gmail.com>
2020-10-22 00:41:39 +0630

    htetaungkhant

--- app.js
+++ app.js
@@ -351,6 +351,26 @@ app.get('/admin/merchant/entrylist', async (req,res) => {
     const outputs = await Promise.all(promises);
 
     console.log(outputs);
+    outputs.forEach(ordersSnapshot => {
+        if(ordersSnapshot.empty) {
+            console.log('No matching documents.');
+            return;
+        }
+        ordersSnapshot.forEach(doc1 => {
+            let user = {};
+            user.id = doc1.id;
+            user.name = doc1.data().name;
+            user.phone = doc1.data().phone;         
+            user.address = doc1.data().address;
+            user.corn_type = doc1.data().corn_type;
+            user.corn_qty = doc1.data().corn_qty;
+            user.wanted_price = doc1.data().wanted_price;
+            user.comment = doc1.data().comment;
+            user.received_date = doc1.data().received_date;   
+            data.push(user); 
+            console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaa') ;
+        }); 
+    })
 
     console.log('start render');
     res.render('merch-entryList.ejs', {data: data});    
