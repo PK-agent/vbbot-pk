0356d2d62aea65053bba42b20c4c8e711c18796b
Htet Aung Khant <chitkogyi19950@gmail.com>
2020-10-21 23:05:24 +0630

    htetaungkhant

--- app.js
+++ app.js
@@ -342,8 +342,8 @@ app.get('/admin/merchant/entrylist', async (req,res) => {
     let data = [];
 
     console.log(userSnapshot);
-    
-    userSnapshot.forEach( async doc => {
+
+    for(const doc of userSnapshot){
         const ordersRef = db.collection('users').doc(doc.id).collection('orders');
         console.log('kkkkkkkkkkkkkkkk'); 
         const ordersSnapshot = await ordersRef.get();
@@ -365,8 +365,33 @@ app.get('/admin/merchant/entrylist', async (req,res) => {
             user.received_date = doc1.data().received_date;   
             data.push(user); 
             console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaa') ;
-        });                   
-    });
+        });
+    }
+    
+    // userSnapshot.forEach( async doc => {
+    //     const ordersRef = db.collection('users').doc(doc.id).collection('orders');
+    //     console.log('kkkkkkkkkkkkkkkk'); 
+    //     const ordersSnapshot = await ordersRef.get();
+    //     console.log('ffffffffffffffffff');
+    //     if(ordersSnapshot.empty) {
+    //         console.log('No matching documents.');
+    //         return;
+    //     }
+    //     ordersSnapshot.forEach(doc1 => {
+    //         let user = {};
+    //         user.id = doc1.id;
+    //         user.name = doc1.data().name;
+    //         user.phone = doc1.data().phone;         
+    //         user.address = doc1.data().address;
+    //         user.corn_type = doc1.data().corn_type;
+    //         user.corn_qty = doc1.data().corn_qty;
+    //         user.wanted_price = doc1.data().wanted_price;
+    //         user.comment = doc1.data().comment;
+    //         user.received_date = doc1.data().received_date;   
+    //         data.push(user); 
+    //         console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaa') ;
+    //     });                   
+    // });
     console.log('start render')
     res.render('merch-entryList.ejs', {data: data});    
 });
