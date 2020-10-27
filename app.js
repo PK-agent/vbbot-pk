38a9bbdcb6fba847026f9cbca0d8ce5a5c54d542
PK-agent <zayar9786@gmail.com>
2020-10-27 20:44:42 +0630

    merchant/book/inventory add firestore

--- app.js
+++ app.js
@@ -293,7 +293,7 @@ app.get('/merchant/book-inventory', async(req,res) => {
 app.post('/merchant/book-inventory', async (req,res) => {  
    
     let today = new Date();
-    let user_id = req.body.currentUser.id;
+    let user_id = req.body.user_id;
 
     let data = {
         created_on:today,
