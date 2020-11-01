1baba22b7ca56ab8d1852bcda8ae80c175866635
PK-agent <zayar9786@gmail.com>
2020-11-01 16:23:55 +0630

    form change

--- app.js
+++ app.js
@@ -408,8 +408,8 @@ app.post('/admin/merchant/book-list', async (req,res) => {
 //staff/merchant/entrylist
 
 app.get('/staff/merchant/inventory-list', async (req,res) => {
-    const merchantsRef = db.collection('merchant-books').doc(docId);
-    const booksSnapshot = await merchantsRef.where('already_confirmed', '==', ture).limit(1).get();
+    const staffsRef = db.collection('merchant-books');
+    const booksSnapshot = await staffsRef.where('already_confirmed', '==', ture).limit(1).get();
     // const snapshot = await usersRef.get();
     let data = [];
     if (booksSnapshot.empty) {
