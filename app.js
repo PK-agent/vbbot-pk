3a204404984633de77764d930de899cb32faac71
PK-agent <zayar9786@gmail.com>
2020-11-15 08:30:03 +0630

    zayarkyaw

--- app.js
+++ app.js
@@ -522,7 +522,7 @@ app.post('/staff/merchant/add-inventory', async (req,res) => {
              .then(res => res.json())
              .then(json => console.log('JSON', json));
              
-             const message = new TextMessage(`Please choose more actions...`, staffKeyboard,null,null,null,3);  
+             const message = new TextMessage(`Please choose more actions...`, staffKeyboard,null,null,null,2);  
              
              bot.sendMessage(currentUserProfile, message);             
         })
